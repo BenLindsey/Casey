6e7a5ecef94d30a18d014835b80fa84243e9af61
Ben Lindsey <ben.lindsey12@imperial.ac.uk>
2014-11-20 14:16:58 +0000

    inti funcs

---
+++ retain.m
@@ -0,0 +1,7 @@
+function [ cbr ] = retain( cbr, solvedcase  )
+%RETAI Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ reuse.m
@@ -0,0 +1,7 @@
+function [ solvedcase ] = reuse( similarcase, newcase )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
