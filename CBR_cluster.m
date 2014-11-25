638c0e187b8fa8422f9eb2575158961e8e33eefc
Cristina Rossi <cr1311@pixel11.doc.ic.ac.uk>
2014-11-25 13:01:12 +0000

    clustclass

--- CBR_cluster.m
+++ CBR_cluster.m
@@ -5,45 +5,52 @@ classdef CBR_cluster
     end
     
     methods
-        function this = CBR_cluster()
+        
+       function this = CBR_cluster(x,y)
             
             weights=zeros(6,45);
-            
-               
-        end
-        
-        
-        
-        function [cbr] = CBRinit(x, y)
-      
-     %%          
-        
+                    
             cases={[],[],[],[],[],[]};
             
+            
+           %% steps through samples putting them into 'cases', organized in clusters
            
             for ol=1:length(y)
                 
                
-                %create case in cluster
+                %appends case to {emotion} cell in cluster
                 
-                cases{y(ol)} = [cases{y(ol)} caseClus(getAU(x(ol,:)), y(ol))]; 
+                cases{y(ol)} = [cases{y(ol)} caseClus(getAU(x(ol,:)), y(ol))]; %caseCLus creates new case with xAU and y as inputs
                                
                 % update weights of cluster
                 
-                weights(y(ol),cases{y(ol)}(end).AUs) = weights(y(ol),cases{y(ol)}(end).AUs) + 1;
-                                     
-                % case already known
+                weights(y(ol),cases{y(ol)}(end).AUs) = weights(y(ol),cases{y(ol)}(end).AUs) + 1; % adds +1 to all (row=emotion, col=+ve AU for sample) of weights
+                            
+              
+            
+                %% for every new sample, it checks the same exact sample does not already exists 
                               
-                for il=1:length( cases{y(ol)} )
-                    if ( isequal  (cases{y(ol)}(end).AUs, cases{y(ol)}(il).AUs)  && (length( cases{y(ol)} )~=il) )
-                        cases{y(ol)}(il).Typicality = cases{y(ol)}(il).Typicality +1;
-                        cases{y(ol)}(end)=[];
-                        break;
-                    end 
+               for il=1:length( cases{y(ol)} ) % steps through all cases classified as emotion of sample
+                    
+                        %  if sample case (<-end) have same AUs as case(il) (for emotion)
+                        % AND il th case is not the last case (ie compraing case to itself) 
+                                        % * if put condition in 'for', problem for lenght=1
+                       
+                        if ( isequal  (cases{y(ol)}(end).AUs, cases{y(ol)}(il).AUs)  && (length( cases{y(ol)} )~=il) )
+                            
+                            cases{y(ol)}(il).Typicality = cases{y(ol)}(il).Typicality +1;   % increases typic. for first case
+                            cases{y(ol)}(end)=[];   % delete second case
+                            
+                            break;  %out of for
+                        end 
                 end
                 
+            end     
+                
+                this.cases=cases;
+                this.weights=weights;
              
-           end
+       end
                
       %%          
                 
@@ -52,6 +59,15 @@ classdef CBR_cluster
                 % indexing ??
                 
             % normalize weights
+            
+               
+
+        
+        
+        function [cbr] = CBRinit_clus(x, y)
+      
+     %%          
+
             
             
          end        
